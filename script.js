77c8d231a84ee457de45e1be3f0ffe1e3abbef4d
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-05-28 12:44:55 +0300

    bir nokta tanimlamak ya da rota cizmek

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 //haritayi ekrana yazdirma ve baslangic koordinatlarini verme 
 
 
-var map = L.map('mapHarita').setView([39.9334, 32.8597],5);
+var map = L.map('mapHarita').setView([38,32],5);
 //setView dedigimiz olay baslangic koordinatlari , 13 ise zoom olayi 
 
 var basemap =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    attribution: "TRAVEL",
+    maxZoom:20,
+    subdomains:["mt0", "mt1", "mt2", "mt3"]
+})
+.addTo(map);
 
-var marker  = L.marker([39.9334, 32.8597]).addTo(map)
+var marker  = L.marker([38,32]).addTo(map)
     //.bindPopup("This is Vodafone Park")
     //.openPopup(); // locasyonn isareti
+
+
+
+
+//bir nokta tanimlamak ya da rota cizmek
+
+var nokta1 =  L.marker([38,32]).addTo(map); // herhangi bir nokta tanimlamak icin
+
+var cordList1 = [[38,32],[38,33],[39,27]]; // cizgi olacaklarin koordinatlari aslinda rota
+var cizgi1 = L.polyline(cordList1).addTo(map)
