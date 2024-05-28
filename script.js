a27f210f5076b00c6a5a59924bf591e23dacbc9c
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-05-28 13:05:34 +0300

    enlem ve boylami alma

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //haritayi ekrana yazdirma ve baslangic koordinatlarini verme 
 
 
-var map = L.map('mapHarita').setView([38,32],5);
+var map = L.map('mapHarita').setView([39.9334,32.8597],5);
 //setView dedigimiz olay baslangic koordinatlari , 13 ise zoom olayi 
 
 var basemap =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,16 +12,38 @@ var basemap =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 .addTo(map);
 
-var marker  = L.marker([38,32]).addTo(map)
-    //.bindPopup("This is Vodafone Park")
-    //.openPopup(); // locasyonn isareti
+ var marker  = L.marker([39.9334,32.8597]).addTo(map)
+//     //.bindPopup("This is Vodafone Park")
+//     //.openPopup(); // locasyonn isareti
 
 
 
 
-//bir nokta tanimlamak ya da rota cizmek
+// //bir nokta tanimlamak ya da rota cizmek
 
-var nokta1 =  L.marker([38,32]).addTo(map); // herhangi bir nokta tanimlamak icin
+// var nokta1 =  L.marker([38,32]).addTo(map); // herhangi bir nokta tanimlamak icin
 
-var cordList1 = [[38,32],[38,33],[39,27]]; // cizgi olacaklarin koordinatlari aslinda rota
-var cizgi1 = L.polyline(cordList1).addTo(map)
+// var cordList1 = [[38,32],[38,33],[39,27]]; // cizgi olacaklarin koordinatlari aslinda rota
+// var cizgi1 = L.polyline(cordList1).addTo(map)
+
+
+
+// //fitbounds
+// cordList1.push([39,34]);
+// var polygon1 = L.polygon(cordList1).addTo(map)
+
+// var bboxYakinlastir = polygon1.getBounds();
+// map.fitBounds(bboxYakinlastir);
+
+//sekil olusturma
+// var koords = [[1,1],[9,9]];
+// var dikdortgen = L.rectangle(koords).addTo(map);
+
+
+//tikladigimiz yerin koordinatlarini alma
+
+map.on("click",function(e){
+    debugger;
+    var noktamiz = e.latlng;
+    alert("Enlem "+noktamiz.lat  + "     Boylam " +noktamiz.lng )
+})
